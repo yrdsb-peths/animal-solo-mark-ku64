b5f19136aa20dd6edcfeb978ae858ef253e8ccff
mark-ku64 <mark64ku@gmail.com>
2025-04-29 14:00:58 -0400

    Add right and left arrow key movement for elephant

--- Elephant.java
+++ Elephant.java
@@ -15,6 +15,14 @@ public class Elephant extends Actor
     public void act()
     {
         // Add your action code here.
+    
+    if (Greenfoot.isKeyDown("left"))
+    {
+        move(-1);
+    }
+    else if (Greenfoot.isKeyDown("right"))
+    {
         move(1);
+    }
     }
 }
