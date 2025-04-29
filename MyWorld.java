b85eec3d0da61972375f7a2034839b66ee4ef0f7
mark-ku64 <mark64ku@gmail.com>
2025-04-29 13:56:21 -0400

    Add elephant and create move command

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,18 @@
 import greenfoot.*;
 
+/**
+ * The world is where the elephant lives in
+ * 
+ * @author mark ku
+ * @ version April 29 2025
+ */
+
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
     }
 }
