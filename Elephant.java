7fb62d7356d9258d8d79844d87741a7e6f3b9fae
mark-ku64 <mark64ku@gmail.com>
2025-05-01 20:22:57 -0400

    added elephant sprite image

--- Elephant.java
+++ Elephant.java
@@ -9,7 +9,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound ("elephantscream.mp3");
+    GreenfootImage idle = new GreenfootImage ("images/elephant_idle/idle0.png");
     
+    /**
+     * Constructor - the code that gets run one time when object is created
+     */
+    public Elephant()
+    {
+        setImage(idle);
+    }
     public void act()
     {
         // Add your action code here.
