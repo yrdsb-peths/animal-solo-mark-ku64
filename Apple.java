46a6e0a795e405534c459447d99afe81a80851bb
mark-ku64 <mark64ku@gmail.com>
2025-05-06 14:24:24 -0400

    set image apple

--- Apple.java
+++ Apple.java
@@ -8,6 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Actor
 {
+    public Apple()
+    {
+        setImage("apple.png");
+    }
     /**
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
